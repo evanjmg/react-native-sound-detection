cf950d2c512930dcae1142b47edaabde1a392871
Evan Gillogley <evanjmg@gmail.com>
2019-03-25 18:38:12 +0000

    fix android

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 
-import { NativeModules } from 'react-native';
-
+import { NativeModules, Platform } from 'react-native';
+const isiOS = Platform.OS === 'ios'
 const { RNSoundDetection } = NativeModules;
 
-export default RNSoundDetection;
+const getAudioTrack = (track) => {
+  const audioTrack = track && track.mFormat && track.mFormat.mMap
+  if (audioTrack) {
+    return { ...audioTrack, type: audioTrack.mime.includes('audio') ? 'audio' : 'video' }
+  }
+  return null
+}
+
+const SoundDetection = isiOS ? RNSoundDetection : { getTracks: (url) => {
+  return new Promise((r) => {
+    RNSoundDetection.getTracks(url, (tracks) => {
+      let result = tracks
+      if (result) {
+        result = JSON.parse(tracks).map(getAudioTrack)
+      }
+      r(result || [])
+    })
+  })
+} }
+export default SoundDetection;
